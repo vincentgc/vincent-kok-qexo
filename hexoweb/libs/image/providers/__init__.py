41f2c96ea35d8ae6f2a5b169e39b947c2847e5ca
vincentgc <vincent_gog@163.com>
2024-03-07 08:56:18 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ hexoweb/libs/image/providers/__init__.py
@@ -0,0 +1,20 @@
+"""
+@Project   : image
+@Author    : abudu
+@Blog      : https://www.oplog.cn
+"""
+from . import custom
+from . import s3
+from . import ftp
+from . import dogecloudoss
+from . import alioss
+from . import gitHub
+
+_all_providers = {
+    custom.Main.name: custom,
+    s3.Main.name: s3,
+    ftp.Main.name: ftp,
+    dogecloudoss.Main.name: dogecloudoss,
+    alioss.Main.name: alioss,
+    gitHub.Main.name: gitHub
+}
